8d736efe5eecfd214d1d00a681c81db086e77b19
sofia abbas <abbassofia92@gmail.com>
2022-10-19 16:52:01 -0500

    app.js

--- app.js
+++ app.js
@@ -3,8 +3,26 @@ console.log(
   "Follow the steps in the README.md file to complete the exercises:\n==========\n"
 );
 
-// Exercise 1
+const firstName = "Sofia";
+let lastName = "Abbas";
+var age = 29;
 
-// Exercise 2
+lastName = "Abbas";
+age = 29;
 
-// Exercise 3
+let language = "JavaScript";
+let createdYear = 1995;
+let isCaseSensitive = true;
+
+let price = 19.99;
+let isOnSale = false;
+let salePercentage = 15;
+let stock = 0;
+let inStock = false;
+let selectedSize = "M";
+
+let title = "Name of the Wind";
+let author = "Patrick Rothfuss";
+let pageCount = 722;
+let bookmark = 456;
+let hasRead = true;
